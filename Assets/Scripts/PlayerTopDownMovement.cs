1ca576d202125942e857022920aa4d49bd8e1695
Krishna Tholudur <krishna.tholudur@gmail.com>
2026-03-07 20:37:37 -0800

    top doown movement animations

--- Assets/Scripts/PlayerTopDownMovement.cs
+++ Assets/Scripts/PlayerTopDownMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerTopDownMovement : MonoBehaviour
 {
     Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
 
     [SerializeField] float speed = 5f;
     [SerializeField] GameObject DialogueCanvas;
@@ -12,10 +14,14 @@ public class PlayerTopDownMovement : MonoBehaviour
 
     private bool dialogueOpen = false;
     private Vector2 lastMoveInput;
+    private Vector2 lastDirection;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        lastDirection = Vector2.down;
         DialogueCanvas.SetActive(false);
     }
 
@@ -23,13 +29,39 @@ public class PlayerTopDownMovement : MonoBehaviour
     {
         lastMoveInput = v.Get<Vector2>();
 
+        if (lastMoveInput != Vector2.zero)
+        {
+            lastDirection = lastMoveInput.normalized;
+        }
+
         if (dialogueOpen)
         {
             rb.linearVelocity = Vector2.zero;
+            animator.SetFloat("Speed", 0f);
             return;
         }
 
         rb.linearVelocity = lastMoveInput * speed;
+        float absX = Mathf.Abs(lastDirection.x);
+        float absY = Mathf.Abs(lastDirection.y);
+        if (absX >= absY)
+        {
+            animator.SetFloat("Horizontal", Mathf.Sign(lastDirection.x));
+            animator.SetFloat("Vertical", 0f);
+            spriteRenderer.flipX = lastDirection.x < 0;
+        }
+        else
+        {
+            animator.SetFloat("Horizontal", 0f);
+            animator.SetFloat("Vertical", Mathf.Sign(lastDirection.y));
+            spriteRenderer.flipX = false;
+        }
+        animator.SetFloat("Speed", lastMoveInput.magnitude);
+        
+        if (lastDirection.x > 0)
+            spriteRenderer.flipX = true;
+        else if (lastDirection.x < 0)
+            spriteRenderer.flipX = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
