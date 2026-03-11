e92985b9da9903b68fdd9b34ab64a9e994502783
TheCubeHamster <38799004+TheCubeHamster@users.noreply.github.com>
2026-03-11 16:54:45 -0700

    Basic start screen

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame() {
+        int currentIndex = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(currentIndex + 1);
+    }
+}
